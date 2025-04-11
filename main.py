8b8ede1ea54e68b149715ac1c7f1902d786a8412
Deepanshusharwan <deepanshusharwan35@gmail.com>
2025-04-11 06:35:17 +0530

    made the horsocope bot along with hello and i love you command support

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import telebot
+import requests
+import datetime
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=["start","hello"])
+def send_welcome(message):
+    bot.reply_to(message,f"Hello {message.from_user.first_name} nice to meet you! how are you?")
+
+
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
+
+
+def day_handler(message):
+    sign = message.text
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(
+        message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(
+        sent_msg, fetch_horoscope, sign.capitalize())
+
+
+def get_daily_horoscope(sign: str, day: str) -> dict:
+    """Get daily horoscope for a zodiac sign.
+    Keyword arguments:
+    sign:str - Zodiac sign
+    day:str - Date in format (YYYY-MM-DD) OR TODAY OR TOMORROW OR YESTERDAY
+    Return:dict - JSON data
+    """
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign": sign, "day": day}
+    response = requests.get(url, params)
+
+    return response.json()
+
+
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\n*Sign:* {sign}\sn*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
+
+ilu_commands = ['i love you',
+                'i luv u',
+                'i lub u',
+                'i lob you',
+                'i lob u',
+                ]
+@bot.message_handler(commands=[m for m in ilu_commands]+[m.upper() for m in ilu_commands]+[f'{m.upper()} ❤' for m in ilu_commands] + [f'{m} ❤' for m in ilu_commands])
+def ilu_handler(message):
+    bot.reply_to(message,f'{message.text} too ')
+
+@bot.message_handler(func=lambda msg:True)
+def echo(message):
+    bot.reply_to(message,message.text)
+
+
+bot.infinity_polling()
